ce3a221e5c0717ef1756990b7fc11b3befeab03c
shirogane <hirano.11l1252@gmail.com>
2024-07-03 13:57:10 +0900

    誤って削除した箇所のリバート

--- src/public/js/star.js
+++ src/public/js/star.js
@@ -1,9 +1,23 @@
 const StarRating = window.VueStarRating.default;
 
 Vue.component('star-rating', StarRating);
+<<<<<<< HEAD
 new Vue({
     el: '#app',
     data: {
         rating: 5
     }
 });
+=======
+let app = new Vue({
+  el: '#app',
+  data: {
+    rating: 0
+  },
+  methods: {
+    submitForm() {
+      console.log(this.rating);  // 送信前にratingの値を確認
+    }
+  }
+});
+>>>>>>> parent of a5ff6e7 (Fixed untracked files issue)
